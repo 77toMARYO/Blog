61919d010832dbe4dd004c3c25a8ed00f49a0ca6
terao nana <amnosonoarashi@gmail.com>
2013-11-11 23:34:13 +0900

    adding files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a1bca86845abe247a24b366799a9c29ddce2b74c6ea6a687392e9f747fe232b8e981861d1278dc12d9f02d2b4c36101f292a10b156bb2120a81831d6bebed84c'
